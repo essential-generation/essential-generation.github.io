9ee99158fc03ce6d728f5f18797a69055d7bfaa3
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2018-09-06 16:34:17 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -97,6 +97,6 @@ function number_aaad(){
 function number_aaae(){
   var array_aaae = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50"];
   var arraya_aaae = Math.floor(Math.random() * array_aaae.length);
-  var arrayb_aaae = array_aaaea[arraya_aaae];
+  var arrayb_aaae = array_aaae[arraya_aaae];
   document.getElementById("number_aaae").innerHTML = arrayb_aaae;
 }
